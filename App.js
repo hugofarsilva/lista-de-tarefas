5cee534cfacef728426b99e2ceda05c72c6559d0
hugofarsilva <sucao9@gmail.com>
2024-03-05 12:35:30 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
+import Constants from 'expo-constants'
+import { AntDesign } from '@expo/vector-icons';
+import { useFonts, 
+  Lato_400Regular,
+  Lato_900Black,
+} from '@expo-google-fonts/lato';
+
+const image = require('./resources/bg.jpg');
+
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Lato_400Regular,
+    Lato_900Black,
+  });
+
+  if (!fontsLoaded && !fontError){
+    return null;
+  }
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+
+        <ImageBackground source={image} style={styles.image}>
+          <View style = {styles.coverView}>
+            <Text style = {styles.text}>Lista de Tarefas</Text>
+          </View>
+        </ImageBackground>
+
+        <View style = {styles.tarefaSingle}>
+
+            <View style = {{flex: 1, width: '100%', padding: 10}}>
+              <Text>
+                Minha tarefa número 1 do dia xx do mês xxx
+              </Text>
+            </View>
+
+            <View style = {{alignItems: 'flex-end', flex: 1, padding: 10}}>
+              <TouchableOpacity>
+                <AntDesign name="minuscircleo" size={24} color="black" />
+              </TouchableOpacity>
+            </View>
+
+          </View>
+          
+    </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+    
   },
+
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    width: '100%',
+    height: 80,
+    
+  },
+
+  text: {
+    color: 'white',
+    fontSize: 20,
+    fontFamily: 'Lato_900Black'
+    
+  },
+
+  coverView: {
+    width: '100%',
+    height: 80,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  tarefaSingle: {
+    marginTop: 30,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    flexDirection: 'row',
+    paddingBottom: 10
+  }
 });
